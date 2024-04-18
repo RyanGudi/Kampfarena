7377b6354c073d21dd16565481e3a91fb19dcfa4
Ryan Gaudier <113002432+RyanGudi@users.noreply.github.com>
2024-04-18 16:13:12 -0700

    Attack class

--- src/main/java/attacks/Attack.java
+++ src/main/java/attacks/Attack.java
@@ -1,12 +1,52 @@
 package attacks;
 
+
+
 public class Attack {
     protected String name;
     protected int damage;
     protected int crit;
     protected int accuracy;
+    protected String[] fireAttacks = {"Flamethrower", ""};
+    protected String[] waterAttacks = {"Hydro Pump", "Bubble Beam", "Water Pulse", "Aqua Tail"};
+    protected String[] grassAttacks = {"Razor Leaf", "Solar Beam", "Energy Ball", "Vine Whip"};
+    
+
+
+    public Attack(String name) {
+        this.name = name;
+
+        switch(name) {
+
+            case "fire":
+
+            break;
+
+            case "water":
+
+            break;
+
+            case "grass":
+
+            break;
+        }
+    }
+
+
 
     public String getName() {
         return name;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public int getCrit() {
+        return crit;
+    }
+
+    public int getAccuracy() {
+        return accuracy;
     }
 }

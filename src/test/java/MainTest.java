4a003f91c4360a463b6e2b8b9bfb82a758e14d55
Ryan Gaudier <113002432+RyanGudi@users.noreply.github.com>
2024-04-17 21:15:26 -0700

    Removed Initial Test Method

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -5,17 +5,5 @@ import static org.junit.Assert.assertEquals;
 
 public class MainTest {
 
-    @Test
-    public void testMainPrintsHelloWorld() {
-        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(outContent));
-
-        Main.main(new String[]{});
-
-        String expectedOutput = "Hello World";
-
-        String actualOutput = outContent.toString().trim();
-
-        assertEquals(expectedOutput, actualOutput);
-    }
+    
 }
